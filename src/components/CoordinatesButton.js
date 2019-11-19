225d6f7ed35c1af3162604430ea509a0d6476661
Danny Lee <dreamer@panix.com>
2019-11-18 23:50:22 -0500

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,44 @@
-// Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+export default class CoordinatesButton extends Component {
+    
+    // We cannot set the state because the "props" being sent down to this
+    // component by index.js is a FUNCTION.  That function, which is called
+    // this.props.onReceiveCoordinates takes an argument of x,y coordinates 
+    // and console logs them.  The call in index.js looks like this:
+    // ln 10: ...onReceiveCoordinates={ mouseCoordinates => console.log(mouseCoordinates) }
+    //
+    // Originally I thought I could set the state and then adjust it with setState()
+    // and it would get passed back up.  Then I realized that since it is a function
+    // it just need to be set to the array of X, Y coordinates and it would then
+    // execute the function in index.js.
+
+    // constructor(props) {
+    //     super(props)
+
+    //     this.state = {
+    //         onReceiveCoordinates: props.mouseCoordinates
+    //     }
+    // }
+
+    // onClickHandler = (event) => {
+    //     this.setState({onReceiveCoordinates: [event.clientX, event.clientY]})
+    // }
+    
+    // Here we are setting the function to handle the "onClick" event...it simply
+    // takes the "event" object passed to by the click event handler and then 
+    // gets the event.clientX and event.clientY properties.  A full list of properties
+    // for mouse events is here: https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent
+    onClickHandler = (event) => 
+        this.props.onReceiveCoordinates([event.clientX, event.clientY])
+
+    // just create a button and add an onClick event handler that passes the event to
+    // the "onClickHandler"
+    render() {
+        return (
+            <button onClick={this.onClickHandler}>
+            </button>
+        )
+    }
+
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,35 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+export default class DelayedButton extends Component {
+    
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            delay: this.props.delay
+        }
+    }
+
+    addDelay(event) {
+        let timer = setTimeout(() => this.props.onDelayedClick(event), this.props.delay);
+    }
+    
+    componentWillUnmount() {
+        clearTimeout(timer);
+    }
+
+    onClickHandler = event => {
+        event.persist()
+        this.addDelay(event)
+    };
+
+    render() {
+        return (
+            <button onClick={this.onClickHandler}>
+                Delayed Button
+            </button>
+        )
+    }
+
+}
